fcad4cfbc9c82f8062e19535379f655a75ba4e83
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-06-21 18:26:58 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Redmine Select2 plugin
+
+This plugin add Select2 component to redmine for using other plugins
+
+## Links
+
+http://ivaynberg.github.io/select2/
---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_select2 do
+  name        'Redmine Select2 plugin'
+  description 'Plugin add select2 component to redmine'
+  author      'Undev'
+  version     '0.0.1'
+  url         'https://github.com/Undev/redmine_select2'
+
+  requires_redmine :version_or_higher => '2.1'
+end
